e73a95199ba2e4273442ba0ae8ad05d83bb93f82
jamesjtong <jtong33@gmail.com>
2013-12-23 15:36:39 -0500

    changes url of mailer

--- app/mailers/game_mailer.rb
+++ app/mailers/game_mailer.rb
@@ -2,7 +2,7 @@ class GameMailer < ActionMailer::Base
   def game_email(game, email)
     mail(to: email.strip,
       from: 'gametableco@gmail.com',
-      body: "Hi, your friend invited you to watch/play a game of #{game.name.capitalize}. The link is gametable.co/#{game.secure_room_code}",
+      body: "Hi, your friend invited you to watch/play a game of #{game.name.capitalize}. The link is gametable.co/games/#{game.secure_room_code}",
       content_type: "text/html",
       subject: "Invitation to Game Table Game" 
     )
